5a4a568ce323ceefedcc45d2174c8f5382bdab11
Karol Bochynski <kolboch@gmail.com>
2017-04-03 11:34:57 +0200

    Classification init

---
+++ 02_lab/content.py
@@ -0,0 +1,131 @@
+# --------------------------------------------------------------------------
+# ------------  Metody Systemowe i Decyzyjne w Informatyce  ----------------
+# --------------------------------------------------------------------------
+#  Zadanie 2: k-NN i Naive Bayes
+#  autorzy: A. Gonczarek, J. Kaczmar, S. Zareba
+#  2017
+# --------------------------------------------------------------------------
+
+from __future__ import division
+import numpy as np
+import numpy.core
+
+
+def hamming_distance(X, X_train):
+    """
+    :param X: zbior porownwanych obiektow N1xD
+    :param X_train: zbior obiektow do ktorych porownujemy N2xD
+    Funkcja wyznacza odleglosci Hamminga obiektow ze zbioru X od
+    obiektow X_train. ODleglosci obiektow z jednego i drugiego
+    zbioru zwrocone zostana w postaci macierzy
+    :return: macierz odleglosci pomiedzy obiektami z X i X_train N1xN2
+    """
+    distance_results = np.empty([X.shape[0], X_train.shape[0]])
+    x_array = X.toarray()
+    x_train_array = X_train.toarray()
+    for x_i in range(X.shape[0]):
+        for x_train_j in range(X_train.shape[0]):
+            distance_results[x_i, x_train_j] = sum(np.logical_xor(x_array[x_i, :], x_train_array[x_train_j, :]))
+    return distance_results
+    pass
+
+
+def sort_train_labels_knn(Dist, y):
+    """
+    Funkcja sortujaca etykiety klas danych treningowych y
+    wzgledem prawdopodobienstw zawartych w macierzy Dist.
+    Funkcja zwraca macierz o wymiarach N1xN2. W kazdym
+    wierszu maja byc posortowane etykiety klas z y wzgledem
+    wartosci podobienstw odpowiadajacego wiersza macierzy
+    Dist
+    :param Dist: macierz odleglosci pomiedzy obiektami z X
+    i X_train N1xN2
+    :param y: wektor etykiet o dlugosci N2
+    :return: macierz etykiet klas posortowana wzgledem
+    wartosci podobienstw odpowiadajacego wiersza macierzy
+    Dist. Uzyc algorytmu mergesort.
+    """
+    pass
+
+
+def p_y_x_knn(y, k):
+    """
+    Funkcja wyznacza rozklad prawdopodobienstwa p(y|x) dla
+    kazdej z klas dla obiektow ze zbioru testowego wykorzystujac
+    klasfikator KNN wyuczony na danych trenningowych
+    :param y: macierz posortowanych etykiet dla danych treningowych N1xN2
+    :param k: liczba najblizszuch sasiadow dla KNN
+    :return: macierz prawdopodobienstw dla obiektow z X
+    """
+    pass
+
+
+def classification_error(p_y_x, y_true):
+    """
+    Wyznacz blad klasyfikacji.
+    :param p_y_x: macierz przewidywanych prawdopodobienstw
+    :param y_true: zbior rzeczywistych etykiet klas 1xN.
+    Kazdy wiersz macierzy reprezentuje rozklad p(y|x)
+    :return: blad klasyfikacji
+    """
+    pass
+
+
+def model_selection_knn(Xval, Xtrain, yval, ytrain, k_values):
+    """
+    :param Xval: zbior danych walidacyjnych N1xD
+    :param Xtrain: zbior danych treningowych N2xD
+    :param yval: etykiety klas dla danych walidacyjnych 1xN1
+    :param ytrain: etykiety klas dla danych treningowych 1xN2
+    :param k_values: wartosci parametru k, ktore maja zostac sprawdzone
+    :return: funkcja wykonuje selekcje modelu knn i zwraca krotke (best_error,best_k,errors), gdzie best_error to najnizszy
+    osiagniety blad, best_k - k dla ktorego blad byl najnizszy, errors - lista wartosci bledow dla kolejnych k z k_values
+    """
+    pass
+
+
+def estimate_a_priori_nb(ytrain):
+    """
+    :param ytrain: etykiety dla dla danych treningowych 1xN
+    :return: funkcja wyznacza rozklad a priori p(y) i zwraca p_y - wektor prawdopodobienstw a priori 1xM
+    """
+    pass
+
+
+def estimate_p_x_y_nb(Xtrain, ytrain, a, b):
+    """
+    :param Xtrain: dane treningowe NxD
+    :param ytrain: etykiety klas dla danych treningowych 1xN
+    :param a: parametr a rozkladu Beta
+    :param b: parametr b rozkladu Beta
+    :return: funkcja wyznacza rozklad prawdopodobienstwa p(x|y) zakladajac, ze x przyjmuje wartosci binarne i ze elementy
+    x sa niezalezne od siebie. Funkcja zwraca macierz p_x_y o wymiarach MxD.
+    """
+    pass
+
+
+def p_y_x_nb(p_y, p_x_1_y, X):
+    """
+    :param p_y: wektor prawdopodobienstw a priori o wymiarach 1xM
+    :param p_x_1_y: rozklad prawdopodobienstw p(x=1|y) - macierz MxD
+    :param X: dane dla ktorych beda wyznaczone prawdopodobienstwa, macierz NxD
+    :return: funkcja wyznacza rozklad prawdopodobienstwa p(y|x) dla kazdej z klas z wykorzystaniem klasyfikatora Naiwnego
+    Bayesa. Funkcja zwraca macierz p_y_x o wymiarach NxM.
+    """
+    pass
+
+
+def model_selection_nb(Xtrain, Xval, ytrain, yval, a_values, b_values):
+    """
+    :param Xtrain: zbior danych treningowych N2xD
+    :param Xval: zbior danych walidacyjnych N1xD
+    :param ytrain: etykiety klas dla danych treningowych 1xN2
+    :param yval: etykiety klas dla danych walidacyjnych 1xN1
+    :param a_values: lista parametrow a do sprawdzenia
+    :param b_values: lista parametrow b do sprawdzenia
+    :return: funkcja wykonuje selekcje modelu Naive Bayes - wybiera najlepsze wartosci parametrow a i b. Funkcja zwraca
+    krotke (error_best, best_a, best_b, errors) gdzie best_error to najnizszy
+    osiagniety blad, best_a - a dla ktorego blad byl najnizszy, best_b - b dla ktorego blad byl najnizszy,
+    errors - macierz wartosci bledow dla wszystkich par (a,b)
+    """
+    pass
