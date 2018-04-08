bb5608d14e7e1f87bfe68cd0ec1bc74311907b7e
Karol <kolboch@gmail.com>
2018-04-08 15:40:21 +0200

    forward checking and backtracking for graph colouring L(2,1), and visualization

--- CSP/main.py
+++ CSP/main.py
@@ -1,5 +1,24 @@
 from problems_csp.problems_CSP import GraphColorL21
 from CSP import AlgorithmCSP
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import numpy as np
 
-coloring = GraphColorL21(3, AlgorithmCSP.BACKTRACKING, None)
-coloring.solve()
+problem_size = 5
+cmap = colors.ListedColormap(['lightblue', 'lightgreen', 'yellow', 'orange', 'grey'])
+
+coloring = GraphColorL21(problem_size, AlgorithmCSP.BACKTRACKING, None)
+result = coloring.solve()
+print(result)
+
+img = plt.imshow(result, interpolation='nearest', origin='lower', cmap=cmap)
+plt.colorbar()
+plt.title("Graph colouring L(2, 1)")
+plt.show()
+#
+# colors = 3
+# x = 5
+# y = 5
+# my_list = [[[z for z in range(colors)] for y in range(3)] for x in range(2)]
+# del my_list[0][0][0]
+# print(my_list)
--- CSP/problems_csp/problems_CSP.py
+++ CSP/problems_csp/problems_CSP.py
@@ -6,11 +6,14 @@ import numpy as np
 
 
 class ProblemCSP(ABC):
+    @abstractmethod
     def __init__(self, problem_size, algorithm, heuristic):
         self.n = problem_size
         self.algorithm = algorithm
         self.heuristic = heuristic
         self.matrix = None
+        self.domain = None
+        self.domains_matrix = None
         self.default_value = -1
         super().__init__()
 
@@ -20,37 +23,104 @@ class ProblemCSP(ABC):
 
 
 class GraphColorL21(ProblemCSP):
+    def __init__(self, problem_size, algorithm, heuristic):
+        self.colors_count = 0
+        super().__init__(problem_size, algorithm, heuristic)
+
     def solve(self):
         if self.algorithm == AlgorithmCSP.FORWARD_CHECKING:
-            self.solve_forward_checking()
+            return self.solve_forward_checking()
         elif self.algorithm == AlgorithmCSP.BACKTRACKING:
-            self.solve_backtracking()
+            return self.solve_backtracking()
         else:
             raise ValueError('This value is not supported. Choose one of AlgorithmCSP values.')
         pass
 
     def solve_forward_checking(self):
-        pass
-
-    def solve_backtracking(self):
-        colors_count = 1
+        self.colors_count = 1
         self.matrix = np.full(shape=(self.n, self.n), fill_value=-1, dtype=int)
         solved = False
-        domains = np.full(shape=(self.n, self.n, colors_count), fill_value=[x for x in range(colors_count)], dtype=list)
         while not solved:
-            colors_count += 1
-            del domains[1, 1].tolist()[1]
-            print(domains)
-            solved = True
+            self.init_domains(self.colors_count)
+            solved = self.forward_solve_util(0, 0)
+            self.colors_count += 1
+        return self.matrix
 
-        print(self.matrix)
+    def init_domains(self, max_colors):
+        self.domains_matrix = [[[z for z in range(max_colors)] for _ in range(self.n)] for _ in range(self.n)]
 
-        pass
+    def forward_solve_util(self, x, y):
+        for color in self.domains_matrix[x][y]:
+            self.matrix[x, y] = color
+            self.validate_domains_matrix(x, y, color)
+            if (x == self.n - 1 and y == self.n - 1) or self.forward_solve_util(*(self.next_indices(x, y))):
+                return True
+            self.restore_domains_from(x, y)
+        return False
+
+    def restore_domains_from(self, row, column):
+        # right adjacent
+        if column + 1 < self.matrix.shape[1]:
+            self.domains_matrix[row][column - 1] = [z for z in range(self.colors_count)]
+        # bottom adjacent
+        if row + 1 < self.matrix.shape[0]:
+            self.domains_matrix[row][column - 1] = [z for z in range(self.colors_count)]
+        # lower left
+        if column - 1 >= 0 and row + 1 < self.matrix.shape[0]:
+            self.domains_matrix[row][column - 1] = [z for z in range(self.colors_count)]
+        # lower right
+        if column + 1 < self.matrix.shape[1] and row + 1 < self.matrix.shape[0]:
+            self.domains_matrix[row][column - 1] = [z for z in range(self.colors_count)]
+
+    def validate_domains_matrix(self, row, column, value):
+        # right adjacent
+        if column + 1 < self.matrix.shape[1]:
+            self.domains_matrix[row][column + 1] = [item for item in self.domains_matrix[row][column + 1] if
+                                                    np.absolute(value - item) >= 2]
+        # bottom adjacent
+        if row + 1 < self.matrix.shape[0]:
+            self.domains_matrix[row + 1][column] = [item for item in self.domains_matrix[row + 1][column] if
+                                                    np.absolute(value - item) >= 2]
+        # lower left
+        if column - 1 >= 0 and row + 1 < self.matrix.shape[0]:
+            self.domains_matrix[row + 1][column - 1] = [item for item in self.domains_matrix[row + 1][column - 1] if
+                                                        np.absolute(value - item) >= 2]
+        # lower right
+        if column + 1 < self.matrix.shape[1] and row + 1 < self.matrix.shape[0]:
+            self.domains_matrix[row + 1][column + 1] = [item for item in self.domains_matrix[row + 1][column + 1] if
+                                                        np.absolute(value - item) >= 2]
+
+    def solve_backtracking(self):
+        self.colors_count = 1
+        self.matrix = np.full(shape=(self.n, self.n), fill_value=-1, dtype=int)
+        solved = False
+        while not solved:
+            self.domain = np.arange(0, self.colors_count)
+            solved = self.solve_util(0, 0)
+            self.colors_count += 1
+        return self.matrix
+
+    def solve_util(self, x, y):
+        for color in self.domain:
+            if self.is_valid(x, y, color):
+                self.matrix[x, y] = color
+                if (x == self.n - 1 and y == self.n - 1) or self.solve_util(*(self.next_indices(x, y))):
+                    return True
+                self.matrix[x, y] = self.default_value
+        return False
+
+    def next_indices(self, x, y):
+        if y + 1 < self.n:
+            return x, y + 1
+        elif x + 1 < self.n:
+            return x + 1, 0
+
+    def is_valid(self, row, column, value):
+        return self.check_adjacent_diff_2(row, column, value) and self.check_biases_diff_1(row, column, value)
 
     # in every condition we check if it is already assigned, if not condition is satisfied
-    def check_adjacent_diff_2(self, row, column):
+    def check_adjacent_diff_2(self, row, column, value):
         is_valid = True
-        value = self.matrix[row, column]
         df = self.default_value
         # left adjacent
         if column - 1 >= 0:
@@ -67,26 +137,33 @@ class GraphColorL21(ProblemCSP):
         return is_valid
 
     # in every condition we check if it is already assigned, if not condition is satisfied
-    def check_biases_diff_1(self, row, column):
+
+    def check_biases_diff_1(self, row, column, value):
         is_valid = True
-        value = self.matrix[row, column]
         df = self.default_value
         # upper left
         if column - 1 >= 0 and row - 1 >= 0:
-            is_valid = self.matrix[row - 1, column - 1] == df or np.absolute(self.matrix[row - 1, column - 1]) >= 1
+            is_valid = self.matrix[row - 1, column - 1] == df or np.absolute(
+                self.matrix[row - 1, column - 1] - value) >= 1
         # upper right
-        if is_valid and column + 1 <= self.matrix.shape[1] and row - 1 >= 0:
-            is_valid = self.matrix[row - 1, column + 1] == df or np.absolute(self.matrix[row - 1, column + 1]) >= 1
+        if is_valid and column + 1 < self.matrix.shape[1] and row - 1 >= 0:
+            is_valid = self.matrix[row - 1, column + 1] == df or np.absolute(
+                self.matrix[row - 1, column + 1] - value) >= 1
         # lower left
-        if is_valid and column - 1 >= 0 and row + 1 <= self.matrix.shape[0]:
-            is_valid = self.matrix[row + 1, column - 1] == df or np.absolute(self.matrix[row + 1, column - 1]) >= 1
+        if is_valid and column - 1 >= 0 and row + 1 < self.matrix.shape[0]:
+            is_valid = self.matrix[row + 1, column - 1] == df or np.absolute(
+                self.matrix[row + 1, column - 1] - value) >= 1
         # lower bottom
-        if is_valid and column + 1 <= self.matrix.shape[1] and row + 1 <= self.matrix.shape[0]:
-            is_valid = self.matrix[row + 1, column + 1] == df or np.absolute(self.matrix[row + 1, column + 1]) >= 1
+        if is_valid and column + 1 < self.matrix.shape[1] and row + 1 < self.matrix.shape[0]:
+            is_valid = self.matrix[row + 1, column + 1] == df or np.absolute(
+                self.matrix[row + 1, column + 1] - value) >= 1
         return is_valid
 
 
 class LatinSquare(ProblemCSP):
+    def __init__(self, problem_size, algorithm, heuristic):
+        super().__init__(problem_size, algorithm, heuristic)
+
     def solve(self):
         if self.algorithm == AlgorithmCSP.FORWARD_CHECKING:
             self.solve_forward_checking()
